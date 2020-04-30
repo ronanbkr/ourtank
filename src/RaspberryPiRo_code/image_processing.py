b4229c823f11d8111ab4aae2ef12765f73ea6fcc
barkerr2 <ronan.barker2@mail.dcu.ie>
2020-04-30 04:35:59 +0100

    Live Videdo

--- src/RaspberryPiRo_code/image_processing.py
+++ src/RaspberryPiRo_code/image_processing.py
@@ -44,101 +44,105 @@ def image_processing():
     time.sleep(2)
 
     confidence_limit = 0.5
-    # loop over the frames from the video stream
-    (clientsocket,address)=server_socket.accept()
-    print ('Hey')
     while True:
-        # Take one frame from the video and resize it
-        frame = vs.read()
-        frame = imutils.resize(frame, width=400)
-
-        # grab the frame dimensions and convert it to a blob
-        (h, w) = frame.shape[:2]
-        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-            0.007843, (300, 300), 127.5)
-
-        # pass the blob through the network and return detections and predictions
-        embedder.setInput(blob)
-        detections = embedder.forward()
-
-        trigger = False 
-
-        # loop over the detections
-        for i in np.arange(0, detections.shape[2]):
-            # extract the confidence of the prediction
-            confidence = detections[0, 0, i, 2]
-
-            # make sure that the confidence is high enough
-            if confidence > confidence_limit:
-                # extract the index for the label
-                # compute the (x, y)-coordinates of
-                # the bounding box for the object
-                idx = int(detections[0, 0, i, 1])
-                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
-                output["start x"] = startX
-                output["start y"] = startY
-                output["end x"] = endX
-                output["end y"] = endY
-                final_confidence = confidence * 100
-                output["confidence"] = final_confidence
-                output["detection label"] = CLASSES[idx]
-                trigger = True
-                output["true/false indicator"] = trigger
-                
-                # depending on the confidence, draw the prediction on the frame
-                if confidence < 0.75:
-                    label = "{}: {:.2f}% Investigate".format(CLASSES[idx], final_confidence)
-                    cv2.rectangle(frame, (startX, startY), (endX, endY),
-                        COLORS[idx], 2)
-                    y = startY - 15 if startY - 15 > 15 else startY + 15
-                    cv2.putText(frame, label, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-                    output["confidence label"] = "Investigate"
-                else:
-                    label = "{}: {:.2f}% Confirmed".format(CLASSES[idx], final_confidence)
-                    cv2.rectangle(frame, (startX, startY), (endX, endY),
-                        COLORS[idx], 2)
-                    y = startY - 15 if startY - 15 > 15 else startY + 15
-                    cv2.putText(frame, label, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-                    output["confidence label"] = "Confirmed"
-                
-        # show the output frame
-        #cv2.imshow("FrameServer", frame)
-        #key = cv2.waitKey(1) & 0xFF
-        
-        # For now print output, it will later be sent on to Ronan.
-        #print(output) 
-        
-        
-        #print (frame)
-         # Server sending frame in bytes format
-        
-        frame_data = pickle.dumps([frame,output])
-        size = len(frame_data)
-        p = struct.pack('I', size)
-        frame_data = p + frame_data
-        clientsocket.sendall(frame_data)
-        #clientsocket.close()
-        
-        # For now print output, it will later be sent on to Ronan.
-        #print(output)
-       
-        
-        # Takes a photo of the stream when c is pressed.
-        # Used for testing
-        # if key == ord("c"):
-        #    img_name = "screenshot_{}.png".format(img_counter)
-        #    cv2.imwrite(img_name, frame)
-        #    print("Screenshot captured.")
-        #    img_counter += 1
+        try:
+            (clientsocket,address)=server_socket.accept()
+            print ('Connected')
             
+            # loop over the frames from the video stream
+            while True:
+                # Take one frame from the video and resize it
+                frame = vs.read()
+                frame = imutils.resize(frame, width=400)
 
-        # break once q is pressed
-        #if key == ord("q"):
-        #    break
-    
+                # grab the frame dimensions and convert it to a blob
+                (h, w) = frame.shape[:2]
+                blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+                    0.007843, (300, 300), 127.5)
+
+                # pass the blob through the network and return detections and predictions
+                embedder.setInput(blob)
+                detections = embedder.forward()
+
+                trigger = False 
+
+                # loop over the detections
+                for i in np.arange(0, detections.shape[2]):
+                    # extract the confidence of the prediction
+                    confidence = detections[0, 0, i, 2]
+
+                    # make sure that the confidence is high enough
+                    if confidence > confidence_limit:
+                        # extract the index for the label
+                        # compute the (x, y)-coordinates of
+                        # the bounding box for the object
+                        idx = int(detections[0, 0, i, 1])
+                        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                        (startX, startY, endX, endY) = box.astype("int")
+                        output["start x"] = startX
+                        output["start y"] = startY
+                        output["end x"] = endX
+                        output["end y"] = endY
+                        final_confidence = confidence * 100
+                        output["confidence"] = final_confidence
+                        output["detection label"] = CLASSES[idx]
+                        trigger = True
+                        output["true/false indicator"] = trigger
+                        
+                        # depending on the confidence, draw the prediction on the frame
+                        if confidence < 0.75:
+                            label = "{}: {:.2f}% Investigate".format(CLASSES[idx], final_confidence)
+                            cv2.rectangle(frame, (startX, startY), (endX, endY),
+                                COLORS[idx], 2)
+                            y = startY - 15 if startY - 15 > 15 else startY + 15
+                            cv2.putText(frame, label, (startX, y),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                            output["confidence label"] = "Investigate"
+                        else:
+                            label = "{}: {:.2f}% Confirmed".format(CLASSES[idx], final_confidence)
+                            cv2.rectangle(frame, (startX, startY), (endX, endY),
+                                COLORS[idx], 2)
+                            y = startY - 15 if startY - 15 > 15 else startY + 15
+                            cv2.putText(frame, label, (startX, y),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                            output["confidence label"] = "Confirmed"
+                        
+                # show the output frame
+                #cv2.imshow("FrameServer", frame)
+                #key = cv2.waitKey(1) & 0xFF
+                
+                # For now print output, it will later be sent on to Ronan.
+                #print(output) 
+                
+                
+                #print (frame)
+                 # Server sending frame in bytes format
+                
+                frame_data = pickle.dumps([frame,output])
+                size = len(frame_data)
+                p = struct.pack('I', size)
+                frame_data = p + frame_data
+                clientsocket.sendall(frame_data)
+                #clientsocket.close()
+                
+                # For now print output, it will later be sent on to Ronan.
+                #print(output)
+               
+                
+                # Takes a photo of the stream when c is pressed.
+                # Used for testing
+                # if key == ord("c"):
+                #    img_name = "screenshot_{}.png".format(img_counter)
+                #    cv2.imwrite(img_name, frame)
+                #    print("Screenshot captured.")
+                #    img_counter += 1
+                    
+
+                # break once q is pressed
+                #if key == ord("q"):
+                #    break
+        except ConnectionResetError as err:
+            print (str(err))
     # Socket Connection closed
     server_socket.close()
      
