e59aa82e72cfe963fed8dbc7927b6cc47411a883
barkerr2 <ronan.barker2@mail.dcu.ie>
2020-05-03 17:42:57 +0100

    Control tweaks

--- src/RaspberryRoPi_code/serverControl.py
+++ src/RaspberryRoPi_code/serverControl.py
@@ -8,16 +8,14 @@ def sendCommand():
     server_socket.listen(5)
     while True:        
         (clientsocket,address)=server_socket.accept()
-        print ('Connected')
+        #print ('Connected')
         command = clientsocket.recv(2048).decode('utf-8')
         print (command)
         try:
             send_command(command)
         except Exception as ex:
             print (ex)
-#"020-x1-33-y1-114-x2-219-y2-256-51.562-0-label"
-    
-        
+       
                 
     
 if __name__=='__main__':
